d26e1e71b2e0fb965a92c6eec439ab3399d39108
LwamTeklay <153647972+LwamTeklay@users.noreply.github.com>
2023-12-12 18:06:52 +0100

    Update App.py

--- App.py
+++ App.py
@@ -2,7 +2,39 @@ import streamlit as st
 from fastai.vision.all import *
 from PIL import Image
 import io
+# Homepage content
+st.markdown("""
+# Welcome to the Roses Image Classification App
 
+This app uses advanced AI to identify various species of roses from your images. Whether you're a gardening enthusiast or just curious about the beautiful world of roses, our app can help you identify the following species:
+
+- Red Roses
+- Floribunda Roses
+- Miniature Roses
+- Hybrid Tea Roses
+- Grandiflora Roses
+- Shrub Roses
+- English Roses
+
+## How to Use
+1. Upload an image of a rose (JPG or PNG format).
+2. Wait for the AI to analyze the image.
+3. View the classification results, including the species name and the confidence level of the prediction.
+
+## About the Classification Model
+Our app is powered by a deep learning model trained specifically on these seven types of roses. It combines accuracy with ease of use to bring you a seamless classification experience.
+
+## Privacy and Data Use
+Your privacy is important to us. Uploaded images are only used for classification and are not stored or used for any other purposes.
+
+## Feedback
+Love our app? Have suggestions? Contact us at [your email/contact form].
+
+## Acknowledgments
+Special thanks to those who contributed to the dataset and development of this app.
+
+Discover the variety and beauty of roses with our app!
+""")
 # Load your trained model
 model = load_learner('resnet50_exported.pkl')
 
