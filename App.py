bc37b18cb0a1c47a5346c45292b81bd49d19f7db
LwamTeklay <153647972+LwamTeklay@users.noreply.github.com>
2023-12-12 17:53:57 +0100

    Update App.py

--- App.py
+++ App.py
@@ -6,20 +6,20 @@ import io
 # Load your trained model
 model = load_learner('resnet50_exported.pkl')
 
-
 st.title('Roses Image Classification App')
 
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png"])
 if uploaded_file is not None:
-    image_data = uploaded_file.read()
-    st.image(image_data, caption='Uploaded Image', use_column_width=True)
+    try:
+        # Read the uploaded image
+        image_data = uploaded_file.read()
+        st.image(image_data, caption='Uploaded Image', use_column_width=True)
 
-    # Convert the file to an image
-    pil_image = Image.open(io.BytesIO(image_data))
+        # Convert the file to an image and resize
+        pil_image = Image.open(io.BytesIO(image_data)).resize((224, 224))
 
-    # Depending on your model, you may need additional image processing here
-
-    # Make a prediction
-    pred,pred_idx,probs = model.predict(pil_image)
-    st.write(f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}')
-
+        # Make a prediction
+        pred,pred_idx,probs = model.predict(pil_image)
+        st.write(f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}')
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
